d8c4465a57f4e73ac92ac94479316d527330c4a4
siddhesh <21078949+siddheshkand@users.noreply.github.com>
2018-05-08 01:00:50 +0530

    Added content to site

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from Home.models import Category
+
+
 def home_page(request):
-    return render(request,"home.html")
+    all_category = Category.objects.filter(is_active=True)
+    return render(request, "home.html", {
+        'all_category': all_category,
+        'width_percent': 25
+    })
