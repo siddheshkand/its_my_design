fb97dcaf4f30f389c7de0a30c4801bb602512115
Rahul Shah <rshahshah2890@gmail.com>
2018-05-20 15:28:50 +0530

    Merge branch 'master' of https://github.com/siddheshkand/its_my_design
    # Conflicts:
    #	Home/templates/home.html
    #	Home/templates/work.html

--- its_my_design/settings.py
+++ its_my_design/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '5_92o649k^o2y2$-o%b5#0(p-f@$1ovliws@zqf*7^*xb1z4v-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.154']
 
 # Application definition
 
